24c11b58226d46d4b30de0817faa7134058b1aaf
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-03-26 13:39:42 +0100

    refactor

--- src/main.c
+++ src/main.c
@@ -6,45 +6,24 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/25 16:19:59 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/03/25 19:50:15 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/03/26 13:39:28 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <stdio.h>
 
-// can do this cleaner
-// check for if there are letters inside the string
-static t_status	error_negative(t_arguments arg)
+static char	*get_argument(t_arguments arg)
 {
-	if (arg == NUMBER_OF_PHILOSOPHERS)
-		printf("number_of_philosophers");
-	else if (arg == TIME_TO_DIE)
-		printf("time_to_die");
-	else if (arg == TIME_TO_EAT)
-		printf("time_to_eat");
-	else if (arg == TIME_TO_SLEEP)
-		printf("time_to_sleep");
-	else if (arg == MAX_EAT_COUNT)
-		printf("number_of_times_each_philosopher_must_eat");
-	printf(" cannot be negative\n");
-	return (FAILURE);
-}
+	char *const	arguments[] = {
+		"number_of_philosophers",
+		"time_to_die",
+		"time_to_eat",
+		"time_to_sleep",
+		"number_of_times_each_philosopher_must_eat"
+	};
 
-static t_status	error_zero(t_arguments arg)
-{
-	if (arg == NUMBER_OF_PHILOSOPHERS)
-		printf("number_of_philosophers");
-	else if (arg == TIME_TO_DIE)
-		printf("time_to_die");
-	else if (arg == TIME_TO_EAT)
-		printf("time_to_eat");
-	else if (arg == TIME_TO_SLEEP)
-		printf("time_to_sleep");
-	else if (arg == MAX_EAT_COUNT)
-		printf("number_of_times_each_philosopher_must_eat");
-	printf(" cannot be zero\n");
-	return (FAILURE);
+	return (arguments[arg]);
 }
 
 static t_status	philo_atoi(size_t *num, char *str, t_arguments arg)
@@ -56,7 +35,10 @@ static t_status	philo_atoi(size_t *num, char *str, t_arguments arg)
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	if (str[i] == '-')
-		return (error_negative(arg));
+	{
+		printf("%s cannot be negative\n", get_argument(arg));
+		return (FAILURE);
+	}
 	if (str[i] == '+')
 		i++;
 	while (str[i] != '\0' && str[i] >= 48 && str[i] <= 57)
@@ -65,19 +47,32 @@ static t_status	philo_atoi(size_t *num, char *str, t_arguments arg)
 		i++;
 	}
 	if (*num == 0)
-		return (error_zero(arg));
+	{
+		printf("%s cannot be zero\n", get_argument(arg));
+		return (FAILURE);
+	}
 	return (SUCCESS);
 }
 
 static t_status	init_data(t_data *data, int argc, char **argv)
 {
-	printf("philo_atoi return: %d\n", philo_atoi(&data->number_of_philosophers, argv[1], NUMBER_OF_PHILOSOPHERS));
-	printf("philo_atoi return: %d\n", philo_atoi(&data->time_to_die, argv[2], TIME_TO_DIE));
-	printf("philo_atoi return: %d\n", philo_atoi(&data->time_to_eat, argv[3], TIME_TO_EAT));
-	printf("philo_atoi return: %d\n", philo_atoi(&data->time_to_sleep, argv[4], TIME_TO_SLEEP));
+	t_status	status;
+
+	status = SUCCESS;
+	if (philo_atoi(&data->num_of_philo, argv[1], NUM_OF_PHILO) == FAILURE)
+		status = FAILURE;
+	if (philo_atoi(&data->time_to_die, argv[2], TIME_TO_DIE) == FAILURE)
+		status = FAILURE;
+	if (philo_atoi(&data->time_to_eat, argv[3], TIME_TO_EAT) == FAILURE)
+		status = FAILURE;
+	if (philo_atoi(&data->time_to_sleep, argv[4], TIME_TO_SLEEP) == FAILURE)
+		status = FAILURE;
 	if (argc == 6)
-		printf("philo_atoi return: %d\n", philo_atoi(&data->max_eat_count, argv[5], MAX_EAT_COUNT));
-	return (SUCCESS);
+	{
+		if (philo_atoi(&data->max_eat_count, argv[5], MAX_EAT_COUNT) == FAILURE)
+			status = FAILURE;
+	}
+	return (status);
 }
 
 int	main(int argc, char **argv)
@@ -89,10 +84,5 @@ int	main(int argc, char **argv)
 		if (init_data(&data, argc, argv) == FAILURE)
 			return (EXIT_FAILURE);
 	}
-	printf("%zu\n", data.number_of_philosophers);
-	printf("%zu\n", data.time_to_die);
-	printf("%zu\n", data.time_to_eat);
-	printf("%zu\n", data.time_to_sleep);
-	printf("%zu\n", data.max_eat_count);
 	return (EXIT_SUCCESS);
 }
