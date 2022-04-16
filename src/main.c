98ebd2e3a626d7f1716c6c6fcdc19d185f63b423
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-04-16 17:52:29 +0200

    added usage when no args given

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/25 16:19:59 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/04/03 13:44:02 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/04/16 17:51:12 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -28,5 +29,9 @@ int	main(int argc, char **argv)
 		destroy_data(&data);
 		destroy_philos(&philos);
 	}
+	else
+		printf("Usage:\n%s <number_of_philosophers> \
+<time_to_die> <time_to_eat> <time_to_sleep> [number_of_times_each_\
+philosopher_must_eat]\n", argv[0]);
 	return (EXIT_SUCCESS);
 }
