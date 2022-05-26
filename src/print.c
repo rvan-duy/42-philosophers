7ddce71da0257276aa7a10b1ca28c590866de288
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-05-26 13:25:41 +0200

    added lock to printfs

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/19 14:56:22 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/05/20 18:05:46 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/05/26 13:23:16 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,9 @@ void	print_dead(t_philo *p, const t_timestamp current_time)
 	pthread_mutex_lock(&p->data->philo_died_lock);
 	p->data->a_philo_died = true;
 	pthread_mutex_unlock(&p->data->philo_died_lock);
+	pthread_mutex_lock(&p->data->print_lock);
 	printf("%zu %zu died\n", current_time, p->seat);
-	// pthread_mutex_lock(&p->data->print_lock);
-	// util_putnbr_fd(current_time, STDOUT_FILENO);
-	// util_putchar_fd(' ', STDOUT_FILENO);
-	// util_putnbr_fd(p->seat, STDOUT_FILENO);
-	// util_putstr_fd(" died", STDOUT_FILENO);
-	// util_putchar_fd('\n', STDOUT_FILENO);
-	// pthread_mutex_unlock(&p->data->print_lock);
+	pthread_mutex_unlock(&p->data->print_lock);
 }
 
 bool	is_dead(t_philo *p)
@@ -41,20 +36,12 @@ bool	is_dead(t_philo *p)
 
 bool	print_message(const char *message, t_philo *p)
 {
-	// t_timestamp	current_time;
-
 	if (is_dead(p) == false)
 	{
-		// pthread_mutex_lock(&p->data->print_lock);
-		printf("%zu %zu %s\n", get_timestamp(p->data->start_time), p->seat, message);
-		// current_time = get_timestamp(p->data->start_time);
-		// util_putnbr_fd(current_time, STDOUT_FILENO);
-		// util_putchar_fd(' ', STDOUT_FILENO);
-		// util_putnbr_fd(p->seat, STDOUT_FILENO);
-		// util_putchar_fd(' ', STDOUT_FILENO);
-		// util_putstr_fd(message, STDOUT_FILENO);
-		// util_putchar_fd('\n', STDOUT_FILENO);
-		// pthread_mutex_unlock(&p->data->print_lock);
+		pthread_mutex_lock(&p->data->print_lock);
+		printf("%zu %zu %s\n", get_timestamp(p->data->start_time), \
+					p->seat, message);
+		pthread_mutex_unlock(&p->data->print_lock);
 		return (true);
 	}
 	return (false);
